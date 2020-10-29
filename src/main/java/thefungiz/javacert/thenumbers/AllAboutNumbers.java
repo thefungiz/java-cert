65baf6e86ab4a178eabe422cad18cf2d058e424f
Derek Zeller <derek.zeller@nelnet.net>
2020-10-29 16:16:34 -0500

    sexigesimal numbers?

--- src/main/java/thefungiz/javacert/thenumbers/AllAboutNumbers.java
+++ src/main/java/thefungiz/javacert/thenumbers/AllAboutNumbers.java
@@ -3,30 +3,43 @@ package thefungiz.javacert.thenumbers;
 public class AllAboutNumbers {
 
     public static void main(String[] args) {
-        println("Here's some info on numbers");
-        println("The floating types:");
-        printNumberInfo(Float.class, Float.MIN_VALUE, Float.MAX_VALUE);
-        printNumberInfo(Double.class, Double.MIN_VALUE, Double.MAX_VALUE);
-        println("The integer types:");
-        printNumberInfo(Byte.class, Byte.MIN_VALUE, Byte.MAX_VALUE);
-        printNumberInfo(Short.class, Short.MIN_VALUE, Short.MAX_VALUE);
-        printNumberInfo(Integer.class, Integer.MIN_VALUE, Integer.MAX_VALUE);
-        printNumberInfo(Long.class, Long.MIN_VALUE, Long.MAX_VALUE);
-        //whatIf();
-        //printAllChars();
+//        println("Here's some info on numbers");
+//        println("The floating types:");
+//        printNumberInfo(Float.class, Float.MIN_VALUE, Float.MAX_VALUE);
+//        printNumberInfo(Double.class, Double.MIN_VALUE, Double.MAX_VALUE);
+//        println("The integer types:");
+//        printNumberInfo(Byte.class, Byte.MIN_VALUE, Byte.MAX_VALUE);
+//        printNumberInfo(Short.class, Short.MIN_VALUE, Short.MAX_VALUE);
+//        printNumberInfo(Integer.class, Integer.MIN_VALUE, Integer.MAX_VALUE);
+//        printNumberInfo(Long.class, Long.MIN_VALUE, Long.MAX_VALUE);
+
+//        doingBytes();
+
+//        whatIf();
+//        printAllChars();
+    }
+
+    private static void doingBytes() {
+//        byte it = 0b00000000;
+//        for (int i = 0; i < 256; i++) {
+//            it += (byte)0b00000001;
+//            println(it);
+//        }
+//        println((byte)0b10000001);
     }
 
     private static void whatIf() {
         println("What if you go overboard?");
         println("let's see");
+//        println((byte)-1 + 0b00000001); // 0b11111111 + 0b00000001 = ?
         println("Byte.MAX_VALUE + 1 = " + (byte)(Byte.MAX_VALUE + 1)); // https://stackoverflow.com/questions/5193883/how-do-you-specify-a-byte-literal-in-java
     }
 
     private static void printAllChars() {
         println("");
         println("Here's some info on chars");
-        for (int i = 0; i <= Character.MAX_VALUE; i++) {
-            println(i + ": " + (char)i);
+        for (char i = 0; i < Character.MAX_VALUE; i = (char)((byte)i + 0b00000001)) {
+            println((byte)i + ": " + i);
         }
     }
 
